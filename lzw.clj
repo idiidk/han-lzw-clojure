b92699d31422b2e12c2e2b38e55ffd3749a614d0
Vincent Brouwer <dviide@protonmail.com>
2022-09-27 12:10:02 +0200

    Working implementation

--- lzw.clj
+++ lzw.clj
@@ -10,8 +10,8 @@
 
 (defn compress [text] ; Take arbitrary text as input
   (loop [t (seq text) ; Loop through input sequence char by char
-    r '() ; Current character
-    w '() ; Next character
+    r () ; Result
+    w () ; Next character
     dict (makeCompressDict) ; Dictionary of ASCII chars (0-255)
     s 256] ; Index
   (let [c (first t)] ; Set c to first character of the sequence of chars
@@ -23,24 +23,19 @@
         (reverse (if w (cons (get dict w) r) r)))))) ; If first character doesnt exist reverse
 
 (defn decompress [bytes] ; Takes a list of bytes as input
-  (let [f (char (first bytes))] ; Used as first character in output
-    (loop [word (rest (seq bytes)) ; Contains content of bytes parameter without first value
-            previousCharacter () ; Used as buffer to keep track of previous character in function
+  (let [f (char (first bytes))] ; Used as first character in rput
+    (loop [word (rest (seq bytes)) ; Contains content of bytes parameter withr first value
+            p (str f) ; Used as buffer to keep track of previous character in function
             dict (makeDecompressDict) ; Dictionary that contains all ASCII characters
-            index 256 ; Index for new values in dictionary
-            out ()] ; Output of function
+            s 256 ; s for new values in dictionary
+            r (str f)] ; Result buffer of function
       (let [currentCharacter (first word)] ; Current character that contains first value of word variable
         (if currentCharacter ; Checks if currentCharacter is not nil
           (if (contains? dict currentCharacter) ; Check if currentCharacter is already in dict
             (recur (rest word) (apply str (get dict currentCharacter 0)) ; If in dictionary, recursively call function with currentCharacter
-                    (assoc dict index (str previousCharacter (first (apply str (get dict currentCharacter 0)))))
-                    (inc index) (cons (apply str (get dict currentCharacter 0)) out))
+                    (assoc dict s (str p (first (apply str (get dict currentCharacter 0)))))
+                    (inc s) (cons (apply str (get dict currentCharacter 0)) r))
             nil) ; If not in dictionary, return nil
-          (apply str f (reverse out))))))) ; If currentCharacter is nil, outputs firstCharacterOutput variable with the reversed output of the out variable as a string, both in combination contain the complete sentence
-
-; (print (makeDecompressDict))
-
-; (defn decompress [#^String text]
-;   ()
+          (apply str (reverse r))))))) ; If currentCharacter is nil, rputs firstCharacterrput variable with the reversed rput of the r variable as a string, both in combination contain the complete sentence
 
 (print (decompress (compress "TOBEORNOTTOBEORTOBEORNOT")))
